c63d046ea5d10c22b345563154807b23718e7796
joaquin <adritmurillo>
2025-10-27 23:27:27 -0500

    Conexion del backend con el frontend: Login

---
+++ src/main/java/com/certify/backend/configuracion/ConfiguracionWeb.java
@@ -0,0 +1,24 @@
+package com.certify.backend.configuracion;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ConfiguracionWeb {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Permite CORS para toda tu API
+                        .allowedOrigins("http://localhost:5173") // URL de tu frontend (ajusta el puerto si es diferente)
+                        .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
